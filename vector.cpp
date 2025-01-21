f26e33e8798d4c33f0516a6ee0d074120650b25b
Maya Stelzer <maya@bipper.net>
2025-01-21 12:53:08 -0800

    removed part 1

--- vector.cpp
+++ vector.cpp
@@ -1,43 +1,5 @@
-#include <iostream>
 #include <vector>
-#include <chrono>
 
-template<typename T>
-class DynamicArray {
-private:
-    T* arr;
-    int size;
-    int capacity;
-
-    void resize() {
-        // TODO: Implement resize function
-    }
-public:
-    // Constructor
-    DynamicArray() {
-        // TODO: Initialize the array with a default size
-    }
-
-    // Destructor
-    ~DynamicArray() {
-        // TODO: Clean up allocatd memory
-    }
-
-    // Add an element to the end of the array
-    void push_back(T element) {
-        //TODO: Implement push_back function
-    }
-
-    // Remove element at the specified index
-    void remove_at(int index) {
-        //TODO: Implement remove_at function
-    }
-
-    // Get the element at the specified index
-    T get_at(int index) {
-        //TODO: Implement get_at function
-    }
-};
 
 void part2() {
     //TODO: Implement part 2
